05cab6371ed2e5fd42b89d2932ab8f5eef2bf16f
zhukai <251201337@qq.com>
2016-11-29 23:12:53 +0800

    handwritetest

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,4 +1,5 @@
 from numpy import *
+from os import listdir
 import operator
 def createDataset():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -41,6 +42,54 @@ def autoNorm(dataSet):
     ranges = maxVals -minVals
     normDataSet = zeros(shape(dataSet))
     m = dataSet.shape[0]
-    normDataSet = dataSet - title(minVals,(m,1))
-    normDataSet = normDataSet/title(ranges,(m,1))
+    normDataSet = dataSet - tile(minVals,(m,1))
+    normDataSet = normDataSet/tile(ranges,(m,1))
     return normDataSet,ranges,minVals
+
+def datingClassTest():
+    hoRatio =0.10
+    datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount =0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print "thle classifier came back with %d,the real answer is:%d"% (classifierResult,datingLabels[i])
+        if (classifierResult != datingLabels[i]):errorCount +=1.0
+    print "the total error rate is: %f" %(errorCount/float(numTestVecs))
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,i*32+j]=int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwlabels=[]
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split(".")[0]
+        classNumStr = int(fileStr.split("_")[0])
+        hwlabels.append(classNumStr)
+        trainingMat[i,:]= img2vector('trainingDigits/%s'%fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount =0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split(".")[0]
+        classNumStr = int(fileStr.split("_")[0])
+        vectorUnderTest = img2vector('testDigits/%s'%fileNameStr)
+        classifierResult = classify0(vectorUnderTest,trainingMat,hwlabels,3)
+        print "thle classifier came back with %d,the real answer is:%d"% (classifierResult,classNumStr)
+        if(classifierResult!=classNumStr):errorCount +=1.0
+    print "the total error rate is: %f" %(errorCount/float(mTest))
+
+    
